cc8957a5a499937fdfcc28d6256fdf5c818d67d8
Akceptor <akceptor.spam@gmail.com>
2014-07-10 08:14:17 +0300

    http://akceptor.org/yak-nakrutyty-vidviduvanist-sajtu-chastyna-6/

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,10 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -12,11 +15,14 @@ import java.util.Random;
  * Created by Akceptor on 22.06.2014.
  */
 public class Main {
-    private static Random randomGenerator = new Random();
 
-    public static void main(String... args) {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+    private static final Random randomGenerator = new Random();
+
+    public static void main(String... args) throws IOException, InterruptedException {
         //User Agents List
         List<String> userAgentsList = new ArrayList<String>();
+        LOGGER.info("Generating User Agents");
         //Add user agent strings here:
         userAgentsList.add("Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0");
         userAgentsList.add("Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Win64; x64; Trident/4.0)");
@@ -24,25 +30,56 @@ public class Main {
         //Init
         FirefoxProfile profile = new FirefoxProfile();
         profile.setPreference("general.useragent.override", randomString(userAgentsList));
+
+        //TOR
+        LOGGER.info("Starting TOR");
+        Process torProcess = Runtime.getRuntime().exec("c:\\Program Files (x86)\\Vidalia Bridge Bundle\\Tor\\tor.exe");
+        //Wait TOR Network
+        LOGGER.info("Waiting for TOR");
+        Thread.sleep(10 * 1000);
+        LOGGER.info("Waiting for TOR ... ");
+        Thread.sleep(20 * 1000);
+        LOGGER.info("Waiting for TOR ... ... ");
+        Thread.sleep(20 * 1000);
+        LOGGER.info("Waiting for TOR ... ... ... ");
+        Thread.sleep(10 * 1000);
+        LOGGER.info("Waiting more than 1 minute already, let's go!");
+
+        //Proxy
+        LOGGER.info("Setting proxy");
+        profile.setPreference("network.proxy.type", 1);
+        profile.setPreference("network.proxy.socks", "localhost");
+        profile.setPreference("network.proxy.socks_port", 9050);
+        LOGGER.info("Creating profile with no cookies");
         WebDriver driver = new FirefoxDriver(profile);
         driver.manage().deleteAllCookies();
 
         //Open site
+        LOGGER.info("Opening IP check site");
+        driver.get("http://www.whatismyip.com");
+
+        LOGGER.info("Opening target site");
         driver.get("http://akceptor.org");
         System.out.println("Cookies: " + driver.manage().getCookies());
 
         //Browse
-        driver.findElement(By.partialLinkText("(частина 1)")).click();
+        LOGGER.info("Browsing");
+        driver.findElement(By.partialLinkText("(частина 5)")).click();
         driver.findElement(By.partialLinkText("Думки вголос")).click();
-        driver.findElement(By.partialLinkText("(частина 2)")).click();
+        driver.findElement(By.partialLinkText("(частина 4)")).click();
         driver.findElement(By.partialLinkText("Думки вголос")).click();
         //Search
+        LOGGER.info("Searching");
         driver.findElement(By.name("s")).sendKeys("selenium");
         driver.findElement(By.name("s")).sendKeys(Keys.RETURN);
         //Featured posts
         driver.findElement(By.className("featured-thumb")).click();
         //Close browser
+        LOGGER.info("Stopping browser");
         driver.close();
+        LOGGER.info("Stoping TOR");
+        torProcess.destroy();
+        LOGGER.info("DONE!");
     }
 
     /**
